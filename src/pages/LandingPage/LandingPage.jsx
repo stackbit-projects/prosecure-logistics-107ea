e229f2ef825c755da2776f62c7a215fb1e5defe8
Stackbit Code Editor <support@stackbit.com>
2021-02-20 13:05:44 +0000

    LandingPage.jsx: changed

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -35,7 +35,7 @@ class LandingPage extends React.Component {
         <Header
           color="transparent"
           routes={dashboardRoutes}
-          brand="Material Kit React"
+          brand="Material Kit Pro Secure Logistics"
           rightLinks={<HeaderLinks />}
           fixed
           changeColorOnScroll={{
